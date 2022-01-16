c15a8ef6f37a455d9eb1bfdd9c1e95c225367a0b
godgo <godgod153@gmail.com>
2022-01-16 20:43:22 +0900

    section5 완료

--- app/src/main/java/com/cookandroid/shoppi_android/MainActivity.kt
+++ app/src/main/java/com/cookandroid/shoppi_android/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.cookandroid.shoppi_android
 
+import android.app.LoaderManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
     }
+
+
 }
